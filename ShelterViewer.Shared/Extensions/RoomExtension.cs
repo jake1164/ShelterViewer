5f01408fbd4cb00967983b6709243801bfde2bee
Jason Jackson <jake1164@hotmail.com>
2024-12-31 08:42:18 -0500

    Added display to dweller table that shows if they are in a room with a max stat.

---
+++ ShelterViewer.Shared/Extensions/RoomExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShelterViewer.Shared.Models;
+
+namespace ShelterViewer.Shared.Extensions;
+
+public static class RoomExtension
+{
+    public static Stats.SpecialStats? ToSpecialStat(this string trait)
+    {
+        return trait.ToLower() switch
+        {
+            "strength" => Stats.SpecialStats.Strength,
+            "perception" => Stats.SpecialStats.Perception,
+            "endurance" => Stats.SpecialStats.Endurance,
+            "charisma" => Stats.SpecialStats.Charisma,
+            "intelligence" => Stats.SpecialStats.Intelligence,
+            "agility" => Stats.SpecialStats.Agility,
+            "luck" => Stats.SpecialStats.Luck,
+            _ => null
+        };
+    }
+}
