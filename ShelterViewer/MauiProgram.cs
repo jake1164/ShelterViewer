5ab71ea229d8cce0d4d06123336153c28f2b8b95
Jake1164 <jake1164@hotmail.com>
2025-10-21 09:43:25 -0400

    Added windows debug only webview debugging.

--- ShelterViewer/MauiProgram.cs
+++ ShelterViewer/MauiProgram.cs
@@ -6,35 +6,63 @@ using ShelterViewer.Shared.Services;
 using ShelterViewer.Shared.Services.VaultServices;
 using ShelterViewer.Services;
 
-namespace ShelterViewer
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
-
-            builder.Services.AddMauiBlazorWebView();
-#if DEBUG
-            builder.Services.AddBlazorWebViewDeveloperTools();
-            builder.Logging.AddDebug();
+#if WINDOWS
+using Microsoft.Web.WebView2.Core;
+using System.Runtime.InteropServices;
 #endif
 
-            builder.Services.AddMudServices();
+namespace ShelterViewer;
 
-            builder.Services.AddSingleton<IRoomTypeLoader, MauiRoomTypeLoader>();
-            builder.Services.AddSingleton<VaultService>();
-            builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddScoped<IUserPreferencesService, UserPreferencesService>();
-            builder.Services.AddScoped<LayoutService>();
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+#if WINDOWS && DEBUG
+    try     {
+        var env = CoreWebView2Environment.GetAvailableBrowserVersionString();
+        System.Diagnostics.Debug.WriteLine($"WebView2 Runtime Version: {env}");
+        Msg.Info($"WebView2 Runtime Version: {env}");
+    }
+    catch (Exception ex)
+    {
+        System.Diagnostics.Debug.WriteLine($"Error retrieving WebView2 Runtime Version: {ex.Message}");
+    }
+#endif
 
-            return builder.Build();
-        }
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddMudServices();
+
+        builder.Services.AddSingleton<IRoomTypeLoader, MauiRoomTypeLoader>();
+        builder.Services.AddSingleton<VaultService>();
+        builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddScoped<IUserPreferencesService, UserPreferencesService>();
+        builder.Services.AddScoped<LayoutService>();
+
+        return builder.Build();
+    }
+}
+
+
+static class Msg
+{
+    [DllImport("User32.dll", CharSet= CharSet.Unicode, SetLastError = false)]
+    private static extern int MessageBoxW(nint hWnd, string text, string caption, uint type);
+
+    public static void Info(string text, string caption = "WebView2 Probe")
+    {
+        MessageBoxW(0, text, caption, 0x00000040);
     }
 }
